151267eb08702ddeaa813ae5e8bf55378aed74cd
Kev Comedia <kevcomedia@gmail.com>
2017-10-15 18:19:53 +0800

    Write Url model

---
+++ models/url.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const urlSchema = new Schema({
+  url: {
+    type: String,
+    required: true,
+    match: /^https?:\/\/.+/,
+  },
+  shortened: {
+    type: String,
+    required: true,
+  },
+});
+
+const Url = mongoose.model('Url', urlSchema);
+
+module.exports = Url;
