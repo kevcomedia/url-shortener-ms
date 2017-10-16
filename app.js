97118d6ce48c2b84d079437c7c2fc99321ebbd07
Kev Comedia <kevcomedia@gmail.com>
2017-10-16 15:28:46 +0800

    Save URL to database

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const app = express();
 
+const Url = require('./models/url');
+
 app.get('/', function(req, res) {
   res.send('Hello World');
+});
+
+app.get(/^\/new\/(https?:\/\/.+)/, function(req, res) {
+  const urlToShorten = req.params[0]
+
+  Url.findOne({url: urlToShorten})
+      .then(function(doc) {
+        if (doc) return Promise.resolve(doc);
+
+        const url = new Url({
+          url: urlToShorten,
+          shortened: '' + (Math.random()), // TODO assign proper value
+        });
+        return url.save();
+      })
+      .then(function({url, shortened}) {
+        return res.send({url, shortened});
+      });
 });
 
 module.exports = app;
