6abb9c9ed9e4c29626f59086423a614d1ef2d4a8
Kev Comedia <kevcomedia@gmail.com>
2017-10-17 11:23:20 +0800

    Move before and after test hooks in separate file

--- test/url.js
+++ test/url.js
@@ -2,42 +2,17 @@
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const mongoose = require('mongoose');
 
 const Url = require('../models/url');
-const app = require('../app');
-
-const testDb = 'test_urlShortener';
-const testDbUri = `mongodb://localhost/${testDb}`;
+const server = require('./index');
 
 chai.should();
 chai.use(chaiHttp);
 
-let server;
-
-before(function startServer(done) {
-  mongoose.Promise = global.Promise;
-
-  mongoose.connect(testDbUri, {useMongoClient: true});
-  mongoose.connection
-      .once('open', () => server = app.listen(8888, done))
-      .on('error', done);
-});
-
-afterEach(function dropTestCollection(done) {
-  mongoose.connection.dropDatabase(testDb, done);
-});
-
-after(function killServer(done) {
-  server.close(function() {
-    mongoose.connection.close(done);
-  });
-});
-
 describe('URL Shortener', () => {
   describe('Parsing URLs', () => {
     it('should accept URLs that start with `http://`', (done) => {
-      chai.request(server)
+      chai.request(server())
           .get('/new/http://example.com')
           .end((err, res) => {
             if (err) done(err);
@@ -50,7 +25,7 @@ describe('URL Shortener', () => {
     });
 
     it('should accept URLs that start with `https://`', (done) => {
-      chai.request(server)
+      chai.request(server())
           .get('/new/https://example.com')
           .end((err, res) => {
             if (err) done(err);
@@ -63,7 +38,7 @@ describe('URL Shortener', () => {
     });
 
     it('should reject if it doesn\'t start with `http://` or `https://`', (done) => {
-      chai.request(server)
+      chai.request(server())
           .get('/new/example.com')
           .end((err, res) => {
             res.should.have.status(400);
@@ -75,7 +50,7 @@ describe('URL Shortener', () => {
 
   describe('Saving URLs', () => {
     it('should return JSON with `original` and `shortened` keys', (done) => {
-      chai.request(server)
+      chai.request(server())
           .get('/new/http://example.com')
           .end((err, res) => {
             if (err) done(err);
@@ -89,7 +64,7 @@ describe('URL Shortener', () => {
     // eslint-disable-next-line max-len
     it('should store the URL and its shortened version in the database', (done) => {
       const urlToShorten = 'http://example.com';
-      chai.request(server)
+      chai.request(server())
           .get('/new/' + urlToShorten)
           .end((err, res) => {
             if (err) done(err);
@@ -107,7 +82,7 @@ describe('URL Shortener', () => {
 
     it('should have the original URL in the response', (done) => {
       const url = 'http://example.com';
-      chai.request(server)
+      chai.request(server())
           .get('/new/' + url)
           .end((err, res) => {
             if (err) done(err);
@@ -120,7 +95,7 @@ describe('URL Shortener', () => {
 
     // eslint-disable-next-line max-len
     it('should reuse the shortened URL if URL is entered more than once', (done) => {
-      chai.request(server)
+      chai.request(server())
           .get('/new/http://example.com')
           .end((err, res) => {
             if (err) done(err);
@@ -128,7 +103,7 @@ describe('URL Shortener', () => {
             res.should.have.status(200);
 
             const originalResponse = res.body;
-            chai.request(server)
+            chai.request(server())
                 .get('/new/http://example.com')
                 .end((err, res) => {
                   if (err) done(err);
@@ -140,7 +115,7 @@ describe('URL Shortener', () => {
     });
 
     it('should display a full URL in the shortened field', (done) => {
-      chai.request(server)
+      chai.request(server())
           .get('/new/http://example.com')
           .end((err, res) => {
             if (err) done(err);
