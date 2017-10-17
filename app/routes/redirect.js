76af3ecd3e8eb700fbe598cef5e4ac95837993ea
Kev Comedia <kevcomedia@gmail.com>
2017-10-17 19:52:03 +0800

    Write redirect logic

---
+++ app/routes/redirect.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router(); // eslint-disable-line new-cap
+
+const Url = require('../../models/url');
+
+router.get(/^\/([A-Za-z0-9]{7})/i, function(req, res) {
+  Url.findOne({shortened: {$regex: req.params[0]}})
+      .then((doc) => {
+        res.redirect(doc.original);
+      })
+      .catch((err) => {
+        res.status(404).send({error: `${req.params[0]} not found`});
+      });
+});
+
+module.exports = router;
--- test/redirect.js
+++ test/redirect.js
@@ -9,22 +9,45 @@ const server = require('./index');
 chai.should();
 chai.use(chaiHttp);
 
-describe('URL Redirect', () => {
+describe('URL Redirect', function() {
+  this.timeout(15000);
+
   beforeEach(function storeUrl(done) {
     chai.request(server())
-        .get('/new/http://example.com')
+        .get('/new/http://www.example.com')
         .end(done);
   });
 
   it('should redirect to the original URL', (done) => {
-    Url.findOne({original: 'http://example.com'})
+    chai.request(server())
+        .get('/new/http://www.example.com')
+        .then(() => {
+          return Url.findOne({original: 'http://www.example.com'});
+        })
         .then((doc) => {
-          return chai.request(server())
-              .get(doc.shortened)
+          // The database doesn't store the port number, so the request URL
+          // needs to be reconstructed to include it.
+          return chai.request('localhost:8888')
+              .get('/' + doc.shortened.substr(-7));
         })
         .then((res) => {
           res.should.redirect;
-          res.should.redirectTo('http://example.com');
+          res.should.redirectTo('http://www.example.com/');
+          done();
+        })
+        .catch(done);
+  });
+
+  // eslint-disable-next-line max-len
+  it('should respond with an error message if shortened URL is not found', (done) => {
+    Url.remove({})
+        .then(() => {
+          return chai.request('localhost:8888').get('/abcdefg');
+        })
+        .catch((err) => {
+          err.should.be.not.null;
+          err.should.have.status(404);
+          err.response.body.should.have.property('error');
           done();
         });
   });
