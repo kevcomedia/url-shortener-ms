8e508a5ddb7a2626ed216f3ea71510728dd8f58f
Kev Comedia <kevcomedia@gmail.com>
2017-10-17 10:24:25 +0800

    Connect to database in index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 process.env.NODE_ENV = 'default';
 
+const mongoose = require('mongoose');
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
-app.listen(port);
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/urlShortener', {useMongoClient: true});
+mongoose.connection
+    .once('open', () => app.listen(port))
+    .on('error', console.error); // eslint-disable-line no-console
