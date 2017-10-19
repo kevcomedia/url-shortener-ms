85374f6aace365d5599462df1e2e66c2f6496af8
Kev Comedia <kevcomedia@gmail.com>
2017-10-19 18:19:30 +0800

    Respond with 400 for improper shortened URLs

--- app/routes/redirect.js
+++ app/routes/redirect.js
@@ -3,7 +3,7 @@ const router = express.Router(); // eslint-disable-line new-cap
 
 const Url = require('../../models/url');
 
-router.get(/^\/([A-Za-z0-9]{7})/i, function(req, res) {
+router.get(/^\/([A-Za-z0-9]{7})$/i, function(req, res) {
   Url.findOne({shortened: {$regex: req.params[0]}})
       .then((doc) => {
         res.redirect(doc.original);
@@ -11,6 +11,10 @@ router.get(/^\/([A-Za-z0-9]{7})/i, function(req, res) {
       .catch((err) => {
         res.status(404).send({error: `${req.params[0]} not found`});
       });
+});
+
+router.use('*', function(req, res) {
+  res.sendStatus(400);
 });
 
 module.exports = router;
