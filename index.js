4872c5c16d689e4ecd00fef31025fb9a5696f6bd
Kev Comedia <kevcomedia@gmail.com>
2017-10-22 18:43:24 +0800

    Use environment variable for db URI

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
+const mongodbUri = process.env.MONGODB_URI;
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/urlShortener', {useMongoClient: true});
+mongoose.connect(mongodbUri, {useMongoClient: true});
 mongoose.connection
     .once('open', () => app.listen(port))
     .on('error', console.error); // eslint-disable-line no-console
